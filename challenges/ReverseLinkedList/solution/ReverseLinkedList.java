4072aa028567908d6ce0589f76eb6245a9d17a08
shrutivanw <shruti.dalvi@gmail.com>
2020-02-15 08:30:04 -0500

    one solution

---
+++ challenges/ReverseLinkedList/solution/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        head = previous;
+        
+        return head;
+    }
+}
