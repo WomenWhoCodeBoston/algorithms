32f9f264b557a171ceb09bfb57619bb44d869266
shrutivanw <shruti.dalvi@gmail.com>
2020-02-15 08:46:32 -0500

    java solution to detect cycle

---
+++ challenges/DetectCycle/solution/DetectCycle.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast == null) {
+                return false;
+            }
+            fast = fast.next;
+        }
+        
+        if (slow == fast) {
+            return true;
+        }
+        return false;
+    }
+}
